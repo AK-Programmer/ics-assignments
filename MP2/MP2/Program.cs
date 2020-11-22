0b873e3c0cca0af1c35f02c9dbcb8019533d7a1c
Adar Kahiri <adar.kahiri4@gmail.com>
2020-11-22 01:56:12 -0500

    most of MP2 done

---
+++ MP2/MP2/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MP2
+{
+    class Program
+    {
+        static StreamReader inFile;
+        static StreamWriter outFile;
+        static Stopwatch timer = new Stopwatch();
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            List<bool> results = new List<bool>();
+
+            Console.WriteLine(CheckNerdWord("XYXY"));
+            /*for (int i = 0; i < 10; i++)
+            {
+                words.Add(GenerateNerdWord());
+                results.Add(CheckNerdWord(words[i]));
+            }
+
+            for(int i = 0; i < words.Count)
+            {
+
+            }*/
+            
+            
+
+
+            /*
+            
+
+            timer.Reset();
+            timer.Start();
+
+
+            try
+            {
+                inFile = new StreamReader("input.txt");
+                string currentLine;
+
+                while (true)
+                {
+                    currentLine = inFile.ReadLine();
+
+                    if (currentLine == "")
+                    {
+                        break;
+                    }
+
+                    words.Add(currentLine);
+                    results.Add(CheckNerdWord(currentLine));
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+
+            inFile.Close();
+
+            outFile = new StreamWriter("Kahiri_A.txt");
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                outFile.WriteLine($"{words[i]}:{(results[i] ? "YES" : "NO")}");
+            }
+
+            outFile.Close();
+            timer.Stop();
+            Console.WriteLine($"Execution time: {timer.Elapsed.Days} Days, {timer.Elapsed.Hours} Hours, {timer.Elapsed.Minutes} Minutes, {timer.Elapsed.Seconds} Seconds, {timer.Elapsed.Milliseconds}");
+            */
+        }
+        
+        public static bool CheckNerdWord(string word)
+        {
+
+            if(word == "X")
+            {
+                return true;
+            }
+            else
+            {
+                int numAs = 0;
+                int numBs = 0;
+                int firstALoc = 0;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    switch (word[i])
+                    {
+                        case 'A':
+                            if (numAs == 0)
+                            {
+                                firstALoc = i;
+                            }
+                            numAs++;
+                            break;
+                        case 'B':
+                            numBs++;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (numAs > 0 && numAs == numBs)
+                    {
+                        return CheckNerdWord(word[0..firstALoc] + word[(firstALoc + 1)..i] + word[(i + 1)..]);
+                    }
+                    if (numAs == 0 && word[i] == 'Y')
+                    {
+                        return CheckNerdWord(word[0..i]) && CheckNerdWord(word[(i + 1)..]);
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+
+
+        public static string GenerateNerdWord()
+        {
+            int choice = rnd.Next(0, 3);
+
+            switch(choice)
+            {
+                case 0:
+                    return GenerateNerdWord() + "Y" + GenerateNerdWord();
+                case 1:
+                    return "A" + GenerateNerdWord() + "B";
+                default:
+                    return "X";
+
+            }
+        }
+    }
+}
---
+++ mP11/mP11/Program.cs
@@ -0,0 +1,89 @@
+﻿//Author: Adar Kahiri
+//Project name: mP11
+//File name: Program.cs
+//Date created: Nov 20, 2020
+//Date modified: Nov 20, 2020
+//Description: This program defines a method for calculating fibonacci numbers and keeps track of its execution speed.
+
+using System;
+using System.Diagnostics;
+
+
+namespace mP11
+{
+    class Program
+    {
+        static Stopwatch stopWatch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            int fibNum;
+
+            //Resetting and starting the timer
+            stopWatch.Reset();
+            stopWatch.Start();
+
+            fibNum = CalcFib(46);
+            //Stopping the timer as soon as the Fibonacci number is calculated
+            stopWatch.Stop();
+
+            Console.WriteLine(fibNum);
+
+            //Printing how long it took to calculate the fibonacci number
+            Console.WriteLine(GetTimeOutput(stopWatch));
+
+            Console.WriteLine(MysterySub(6, 2, 5));
+        }
+
+        //Pre: must be a positive integer
+        //Post: returns the sum of the previous two Fibonacci numbers.
+        //Description: this method recursively calculates the value of the nth fibonacci number.
+        public static int CalcFib(int n)
+        {
+            //If n = 1 or n = 2, then the nth fibonacci number is 1 (the fibonacci sequence starts with 1 as its first 2 numbers)
+            if (n <= 2)
+            {
+                return 1;
+            }
+            //Otherwise, recursively return the previous two fibonacci numbers. Those function calls will also recursively return the previous two, until n = 2 or n = 1.
+            else
+            {
+                return CalcFib(n - 1) + CalcFib(n - 2);
+            }
+        }
+
+        //Pre: must be a valid Stopwatch object
+        //Post: returns how much time has elapsed in a nicely formatted fashion
+        //Descrption: This method returns the days, hours, minutes, seconds, and milliseconds that the given timer was running for
+        public static string GetTimeOutput(Stopwatch timer)
+        {
+            TimeSpan ts = timer.Elapsed;
+            int millis = ts.Milliseconds;
+            int seconds = ts.Seconds;
+            int minutes = ts.Minutes;
+            int hours = ts.Hours;
+            int days = ts.Days;
+
+            return "Time- Days:Hours:Minutes:Seconds.Milliseconds:" + days + ":" +
+                                                                      hours + ":" +
+                                                                      minutes + ":" +
+                                                                      seconds + "." +
+                                                                      millis;
+        }
+
+        public static int MysterySub(int a, int b, int c)
+        {
+            if (a <= 3)
+            {
+                return b;
+            }
+            else
+            {
+                return c + MysterySub(a - 2, b, c);
+            }
+        }
+
+
+    }
+
+}
